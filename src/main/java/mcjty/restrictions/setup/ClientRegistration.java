f9dab34dad455cb2b5e3fc9f6645477bf1d60155
McJty <mcjty1@gmail.com>
2019-06-27 15:10:27 +0200

    Some fixes but baked models aren't working yet

--- src/main/java/mcjty/restrictions/setup/ClientRegistration.java
+++ src/main/java/mcjty/restrictions/setup/ClientRegistration.java
@@ -1,12 +1,13 @@
 package mcjty.restrictions.setup;
 
 
+import mcjty.restrictions.Restrictions;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.client.event.ModelRegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod.EventBusSubscriber(Dist.CLIENT)
+@Mod.EventBusSubscriber(modid = Restrictions.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class ClientRegistration {
 
     @SubscribeEvent

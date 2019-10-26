f9153116bbec765e0ecf9b2b6b36e8101b2e2530
McJty <mcjty1@gmail.com>
2019-10-26 15:57:16 +0200

    Better recipe for the glass boots and proper loot tables for the blocks

--- src/main/java/mcjty/restrictions/datagen/LootTables.java
+++ src/main/java/mcjty/restrictions/datagen/LootTables.java
@@ -1,16 +1,21 @@
 package mcjty.restrictions.datagen;
 
+import mcjty.lib.datagen.BaseLootTableProvider;
+import mcjty.restrictions.blocks.ModBlocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.DirectoryCache;
 import net.minecraft.data.LootTableProvider;
 
-public class LootTables extends LootTableProvider {
+public class LootTables extends BaseLootTableProvider {
 
     public LootTables(DataGenerator dataGeneratorIn) {
         super(dataGeneratorIn);
     }
 
     @Override
-    public void act(DirectoryCache cache) {
-    }
+    protected void addTables() {
+        lootTables.put(ModBlocks.ATTRACTOR_BLOCK, createStandardTable("attractor", ModBlocks.ATTRACTOR_BLOCK));
+        lootTables.put(ModBlocks.ONEWAY_BLOCK, createStandardTable("oneway", ModBlocks.ONEWAY_BLOCK));
+        lootTables.put(ModBlocks.PUSHER_BLOCK, createStandardTable("pusher", ModBlocks.PUSHER_BLOCK));
+}
 }
--- src/main/java/mcjty/restrictions/datagen/Recipes.java
+++ src/main/java/mcjty/restrictions/datagen/Recipes.java
@@ -1,18 +1,45 @@
 package mcjty.restrictions.datagen;
 
+import mcjty.lib.datagen.BaseRecipeProvider;
+import mcjty.restrictions.blocks.ModBlocks;
+import mcjty.restrictions.items.ModItems;
+import net.minecraft.advancements.criterion.InventoryChangeTrigger;
+import net.minecraft.block.Blocks;
 import net.minecraft.data.DataGenerator;
 import net.minecraft.data.IFinishedRecipe;
-import net.minecraft.data.RecipeProvider;
+import net.minecraft.data.ShapedRecipeBuilder;
+import net.minecraft.item.Items;
+import net.minecraftforge.common.Tags;
 
 import java.util.function.Consumer;
 
-public class Recipes extends RecipeProvider {
+public class Recipes extends BaseRecipeProvider {
 
     public Recipes(DataGenerator generatorIn) {
         super(generatorIn);
+        group("restrictions");
     }
 
     @Override
     protected void registerRecipes(Consumer<IFinishedRecipe> consumer) {
+        build(consumer, ShapedRecipeBuilder.shapedRecipe(ModBlocks.ATTRACTOR_BLOCK)
+                        .key('f', Items.FEATHER)
+                        .key('P', Blocks.STICKY_PISTON)
+                        .addCriterion("redstone", InventoryChangeTrigger.Instance.forItems(Items.REDSTONE)),
+                "frf", "rPr", "frf");
+        build(consumer, ShapedRecipeBuilder.shapedRecipe(ModBlocks.PUSHER_BLOCK)
+                        .key('f', Items.FEATHER)
+                        .key('P', Blocks.PISTON)
+                        .addCriterion("redstone", InventoryChangeTrigger.Instance.forItems(Items.REDSTONE)),
+                "frf", "rPr", "frf");
+        build(consumer, ShapedRecipeBuilder.shapedRecipe(ModBlocks.ONEWAY_BLOCK)
+                        .key('f', Tags.Items.SLIMEBALLS)
+                        .key('P', Items.RAIL)
+                        .addCriterion("redstone", InventoryChangeTrigger.Instance.forItems(Items.REDSTONE)),
+                "frf", "rPr", "frf");
+        build(consumer, ShapedRecipeBuilder.shapedRecipe(ModItems.GLASSBOOTS)
+                        .key('g', Tags.Items.GLASS_PANES)
+                        .addCriterion("glass", InventoryChangeTrigger.Instance.forItems(Items.GLASS)),
+                "   ", "g g", "G G");
     }
 }

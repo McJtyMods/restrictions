592b8a2fcb5148f2fe27a9c7cce39251943985e6
McJty <mcjty1@gmail.com>
2020-08-29 09:49:18 +0200

    Proper break particles for the generated blockstates. Also better structure for generated data

--- src/main/java/mcjty/restrictions/datagen/BlockStates.java
+++ src/main/java/mcjty/restrictions/datagen/BlockStates.java
@@ -4,7 +4,6 @@ import mcjty.lib.datagen.BaseBlockStateProvider;
 import mcjty.restrictions.Restrictions;
 import mcjty.restrictions.setup.Registration;
 import net.minecraft.data.DataGenerator;
-import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.client.model.generators.ExistingFileHelper;
 
 public class BlockStates extends BaseBlockStateProvider {
@@ -21,27 +20,31 @@ public class BlockStates extends BaseBlockStateProvider {
                 modLoc("block/attractor"),
                 modLoc("block/side"),
                 modLoc("block/side"),
-                modLoc("block/side")));
+                modLoc("block/side"))
+                .texture("particle", modLoc("block/side")));
         orientedBlock(Registration.PUSHER.get(), models().cube("pusher",
                 modLoc("block/side"),
                 modLoc("block/side"),
                 modLoc("block/pusher"),
                 modLoc("block/side"),
                 modLoc("block/side"),
-                modLoc("block/side")));
+                modLoc("block/side"))
+                .texture("particle", modLoc("block/side")));
         orientedBlock(Registration.ONEWAY.get(), models().cube("oneway",
                 modLoc("block/sideone"),
                 modLoc("block/sideone"),
                 modLoc("block/onewayn"),
                 modLoc("block/oneways"),
                 modLoc("block/sideone"),
-                modLoc("block/sideone")));
+                modLoc("block/sideone"))
+                .texture("particle", modLoc("block/sideone")));
         orientedBlock(Registration.ONEWAY_WALL.get(), models().cube("oneway_wall",
                 modLoc("block/sideone"),
                 modLoc("block/sideone"),
                 modLoc("block/sideone"),
                 modLoc("block/side"),
                 modLoc("block/sideone"),
-                modLoc("block/sideone")));
+                modLoc("block/sideone"))
+                .texture("particle", modLoc("block/sideone")));
     }
 }
